a4285a229e42c3c2c3c47c8d122576012119e8c5
万安 <wanan@wanandemacbook-pro-2.local>
2016-04-20 14:14:15 +0800

    Initial Commit

---
+++ swifitday01－基本运算/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swifitday01－基本运算
+//
+//  Created by 万安 on 16/4/20.
+//  Copyright © 2016年 penglei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
